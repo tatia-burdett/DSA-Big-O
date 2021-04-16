09ea9e52bc81bdaca045367ffaf84af516a43df7
Tatia burdett <tatiaburdett@gmail.com>
2021-04-16 10:43:48 -0700

    add drills

---
+++ drills/are-you-here.js
@@ -0,0 +1,12 @@
+function areYouHere(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+      const el1 = arr1[i];
+      for (let j = 0; j < arr2.length; j++) {
+          const el2 = arr2[j];
+          if (el1 === el2) return true;
+      }
+  }
+  return false;
+}
+
+// 0(n^k) Polynominal time -- this function contains a nested loop, requiring two levels of looping, so technically 0(n^2)
---
+++ drills/even-or-odd.js
@@ -0,0 +1,10 @@
+function isEven(value) {
+  if (value % 2 === 0) {
+      return true;
+  }
+  else {
+      return false;
+  }
+}
+
+// O(1)  -- no matter the input size (value), the algorithm only runs once, thus taking about the same amount of time to complete
