944db04c3d1008e4b34b01c4a44748f4500cb4d8
Tatia burdett <tatiaburdett@gmail.com>
2021-04-17 09:26:48 -0700

    big O

--- drills/random-element.js
+++ drills/random-element.js
@@ -2,3 +2,9 @@ function findRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(findRandomElement(arr))
+
+// O(n) -- scales directly with size of input
